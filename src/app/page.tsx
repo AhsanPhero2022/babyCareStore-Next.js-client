16a1973e9b7fe781724a93d5ebe3fbddac334b1d
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-03-27 16:53:07 +0600

    home page error done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import BrandPage from "@/components/shared/brand/page";
 import FlashSalePage from "@/components/shared/flashSale/page";
-import Homepage from "@/components/shared/home/page";
+import HomePage from "@/components/shared/home/page";
+
 import PopularProductsPage from "@/components/shared/popularProducts/page";
 
 const Home = () => {
   return (
     <div>
-      <Homepage />
+      <HomePage/>
       <FlashSalePage />
       <BrandPage />
       <PopularProductsPage/>
--- src/components/shared/Home/page.tsx
+++ src/components/shared/Home/page.tsx
@@ -5,7 +5,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
 
-const Homepage = () => {
+const HomePage = () => {
   const settings = {
     dots: true,
     infinite: true,
@@ -175,4 +175,4 @@ const Homepage = () => {
   );
 };
 
-export default Homepage;
+export default HomePage;
